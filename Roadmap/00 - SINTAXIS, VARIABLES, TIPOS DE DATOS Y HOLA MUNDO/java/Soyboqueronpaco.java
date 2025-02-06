5d5f8de3ed19b89b87638fde975b8713fd961ed1
Your Name <you@example.com>
2025-02-06 13:17:39 +0100

    #00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO - java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Soyboqueronpaco.java
@@ -0,0 +1,25 @@
+public class Soyboqueronpaco {
+    public static void main(String[] args) throws Exception {
+        // URL del sitio oficial de Java
+        // https://www.java.com.es
+        /**
+         * variables y contantes en Java
+         * Primero se pone el tipo de variable
+         * segundo el nombre de la variable
+         * despues de puede declara o dale valor
+         */
+        //numerico 
+        int numero = 10; //variable de numero entero
+        double decimaldoble = 10.00; //decimal con dos cifras siempre va con . en vez ,
+        float decimal = 10.48578F; //siempre se termina con F
+        final float NUMERO_PI =  3.141592F; // contstate valor no se modifica por ejemplo numero pi
+        // tipos booleanos
+        boolean verdad = true;
+        boolean mentira = false;
+        
+        //Tipo de caracteres
+        char a = 'a'; //solo tiene un caracter y va entre ''
+        String miNombre = "Paco"; // es una variable especial no una variable prima pero se usa como si lo fuera y va entre ""
+        System.out.println("Hola desde Java no de javascript");
+    }
+}
